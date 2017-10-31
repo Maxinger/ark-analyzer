ff91805f589619783c1c642c4ca037dddfc357a4
max.kishkevich <max.kishevich@itibo.com>
2017-10-31 18:34:38 +0300

    Voters distribution histogram.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# ark-analyzer
+# ARK Blockchain Analyzer
+
+The project allows to analyze current ARK delegates statistics. It can be helpful for potential delegates to get some insights about attracting new voters. 
+
+## Usage
+ 
+Build the project with Maven:
+
+`mvn clean install`
+
+Run Spring Boot application:
+
+`java -jar target/ark-analyzer-0.1-SNAPSHOT.jar`
+
+Open URL: http://localhost:8080
--- src/main/java/by/maxi/blockchain/ark/DelegateController.java
+++ src/main/java/by/maxi/blockchain/ark/DelegateController.java
@@ -2,6 +2,10 @@ package by.maxi.blockchain.ark;
 
 import by.maxi.blockchain.ark.model.Voter;
 import by.maxi.blockchain.ark.service.DelegateService;
+import org.knowm.xchart.BitmapEncoder;
+import org.knowm.xchart.CategoryChart;
+import org.knowm.xchart.Histogram;
+import org.knowm.xchart.style.CategoryStyler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +13,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
+import java.util.Base64;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,10 +28,12 @@ public class DelegateController {
 
     private static final Logger LOG = LoggerFactory.getLogger(DelegateController.class);
 
+    private static final Long TRILLION = 1000000000000L;
+
     @Autowired
     private DelegateService delegateService;
 
-    @RequestMapping("/delegates")
+    @GetMapping({"/", "/delegates"})
     public String delegatesList(Model model) {
         model.addAttribute("delegates", delegateService.getForgingDelegates());
         return "delegates";
@@ -42,8 +49,35 @@ public class DelegateController {
 
         model.addAttribute("mean", voters.stream().mapToLong(Voter::getBalance).average().getAsDouble());
 
+        try {
+            model.addAttribute("hist", buildChart(voters));
+        } catch (IOException e) {
+            LOG.error("Failed to build a histogram for voters distribution", e);
+        }
+
         model.addAttribute("voters", voters.stream().sorted(Comparator.comparingLong(Voter::getBalance).reversed())
                 .limit(10).collect(Collectors.toList()));
         return "voters";
+    }
+
+    private String buildChart(List<Voter> voters) throws IOException {
+        List<Long> nums = voters.stream().mapToLong(Voter::getBalance).boxed().collect(Collectors.toList());
+        Histogram hist = new Histogram(nums, Math.min(nums.size(), 5));
+
+        CategoryChart chart = new CategoryChart(800, 500);
+        chart.setTitle("Voters distribution");
+        chart.setXAxisTitle("Balances (in trillions)");
+        chart.setYAxisTitle("Number");
+        chart.addSeries("hist",
+                hist.getxAxisData().stream().mapToLong(x -> Math.round(x / TRILLION)).boxed().collect(Collectors.toList()),
+                hist.getyAxisData());
+
+        CategoryStyler styler = chart.getStyler();
+        styler.setHasAnnotations(true);
+        styler.setLegendVisible(false);
+        styler.setXAxisDecimalPattern("0");
+
+        byte[] bytes = BitmapEncoder.getBitmapBytes(chart, BitmapEncoder.BitmapFormat.PNG);
+        return Base64.getEncoder().encodeToString(bytes);
     }
 }
